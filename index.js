7846a15e2d8f84e2bda590141bc84d09fc28edbb
German Simba <germansimbasan@gmail.com>
2020-09-16 09:03:53 -0500

    probando

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-app.set('port',process.env.PORT || 3000);
+//app.set('port',process.env.PORT || 3000);
+let port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
@@ -10,7 +11,8 @@ app.use(express.json());
 
 app.use(require('./src/routes/developer.routes'));
 
-app.listen(3000,()=>{
-    console.log(`server on port ${app.get('port')}`);
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`)
+    //console.log(`server on port ${app.get('port')}`);
 }
 )
